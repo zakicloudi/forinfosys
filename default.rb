0e0de8426b39af49b706252cd7df661c251512f2
Zaki <mohammed.zaki@infotechprism.com>
2017-02-13 02:10:25 -0500

    sping up linux box, chef as provisioner and with ip addr

---
+++ default.rb
@@ -0,0 +1,14 @@
+execute "apt-get update"
+package "apache2" do 
+action :install
+end
+
+service "apache2" do
+        action [:enable, :start]
+	end
+execute "rm -rf /var/www"
+link "/var/www" do
+	to  "/vagrant"
+end
+
+
---
+++ init.sh
@@ -0,0 +1,4 @@
+# to hold our recipeis 
+vagrant init ubuntu/xenial64
+mkdir -p cookbooks/vagrant_la/recipes
+vagrant up
